2b01f67d3e30375cb136d4e7416eea8ccf2b1e4a
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-09 20:02:02 +0000

    added try and except blocks to json file section so that the user gets a message if they try to create an already existing file, added comments explaining all the try block keywords

--- json_with_python.py
+++ json_with_python.py
@@ -10,8 +10,13 @@ car_data = {"name": "tesla", "engine": "electric"}
 car_data_json_string = json.dumps(car_data)
 print(type(car_data_json_string))  # We can see that it has changed type from dict to str
 # This is how we can encode from a dictionary and write to a file (w gives write permissions)
-with open("new_json_file.json", "w") as jsonfile:  # Writes our data to a json file and aliases the file name
-    json.dump(car_data, jsonfile)  # dump all of the data from the car_data variable into the json file
+try:
+    with open("new_json_file.json", "x") as jsonfile:  # Writes our data to a json file and aliases the file name
+        json.dump(car_data, jsonfile)  # dump all of the data from the car_data variable into the json file
+        print("File created!")
+        jsonfile.close()
+except FileExistsError:
+    print("Sorry, this file already exists. Try naming it something different.")
 # json.dump() - creates a stream object and accept a file object to write to
 
 with open("new_json_file.json") as jsonfile:
@@ -24,3 +29,14 @@ with open("new_json_file.json") as jsonfile:
 
 # Json is used heavily in the industry so reading(encoding), writing(decoding), parsing, and converting data
 # are the most commonly utilised options
+
+# exception handling
+# explain what these all do here, add them to README and include exception handling to check if the
+# file is created if not return back the original exception together with customised user friendly message
+# try:  # Lets you test a block of code for errors
+#     doing_this
+# except:  # Lets you handle the error
+#     do_this_if_an_error_is_raised
+# raise TypeOfException("message to output")  # Used to raise a exceptions
+# else:  # Can use this to execute if no exceptions were raised
+# finally:  # Lets you execute code, regardless of the result of the try and except blocks
